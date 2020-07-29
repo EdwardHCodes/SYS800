6ac7525a4cf548e8fb81e97e34a1ae298c28f061
Edward Holcomb <weminecryptocurrency@gmail.com>
2020-07-29 15:45:12 -0500

    Update ssw800_parsing_script.py

--- ssw800_parsing_script.py
+++ ssw800_parsing_script.py
@@ -3,5 +3,10 @@
 #for windows machines you must also download additional dll files
 from comment_parser import comment_parser
 
-#
+
+#for files with c code
 comment_parser.extract_comments('test.c' , mime='text/x-c')
+
+
+#for files with .s extension
+comment_parser.extract_comments('test2.s', mime='text/x-c')
